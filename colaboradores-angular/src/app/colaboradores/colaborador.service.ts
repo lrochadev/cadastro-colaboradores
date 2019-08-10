35141882db06a6fabc771dae4803709003b8bee5
Leonardo Rocha <lrochadev@gmail.com>
2019-08-10 09:33:40 -0300

    get url from enviroment.

--- colaboradores-angular/src/app/colaboradores/colaborador.service.ts
+++ colaboradores-angular/src/app/colaboradores/colaborador.service.ts
@@ -2,7 +2,9 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-const API = 'http://localhost:8080/colaboradores';
+import { environment } from '../../environments/environment';
+
+const API = environment.api;
 
 @Injectable({
   providedIn: 'root'
